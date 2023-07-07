e95a41535eaa484020e0420737accbf432316cfe
aka-nick <bechalrang@gmail.com>
2023-07-07 13:48:41 +0900

    트랜잭션 스크립트 - 빈약한 도메인 객체 - Customer domain 추가

--- src/main/java/org/akanick/moviereservation/domain/Customer.java
+++ src/main/java/org/akanick/moviereservation/domain/Customer.java
@@ -1,6 +1,5 @@
 package org.akanick.moviereservation.domain;
 
-
 import static jakarta.persistence.GenerationType.IDENTITY;
 
 import jakarta.persistence.Column;
@@ -9,22 +8,13 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 
 @Entity
-public class Movie {
+public class Customer {
 
     @Id
     @GeneratedValue(strategy = IDENTITY)
     private Long id;
 
-    @Column(name="title")
-    private String title;
-
-    @Column(name="running_time")
-    private Integer runningTime;
-
-    @Column(name="fee_amount")
-    private Integer feeAmount;
-
-    @Column(name="fee_currency")
-    private Integer feeCurrency;
+    @Column(name = "name")
+    private String name;
 
 }
--- src/main/java/org/akanick/moviereservation/domain/Showing.java
+++ src/main/java/org/akanick/moviereservation/domain/Showing.java
@@ -1,10 +1,11 @@
 package org.akanick.moviereservation.domain;
 
+import static jakarta.persistence.GenerationType.IDENTITY;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
@@ -14,7 +15,7 @@ import java.time.LocalDateTime;
 public class Showing {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = IDENTITY)
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
