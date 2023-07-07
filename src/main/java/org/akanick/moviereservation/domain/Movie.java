9666b9a0cbe729b0cd6452a9e4fca2aba250d135
aka-nick <bechalrang@gmail.com>
2023-07-07 13:37:51 +0900

    트랜잭션 스크립트 - 빈약한 도메인 객체 - Movie domain 추가

---
+++ src/main/java/org/akanick/moviereservation/domain/Movie.java
@@ -0,0 +1,27 @@
+package org.akanick.moviereservation.domain;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+public class Movie {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name="title")
+    private String title;
+
+    @Column(name="running_time")
+    private Integer runningTime;
+
+    @Column(name="fee_amount")
+    private Integer feeAmount;
+
+    @Column(name="fee_currency")
+    private Integer feeCurrency;
+
+}
