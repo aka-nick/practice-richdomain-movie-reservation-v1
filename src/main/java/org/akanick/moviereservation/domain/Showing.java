47e9c3d4a202a80770cb4cd84c632d82d1b51179
aka-nick <bechalrang@gmail.com>
2023-07-07 13:43:30 +0900

    트랜잭션 스크립트 - 빈약한 도메인 객체 - Showing domain 추가

--- src/main/java/org/akanick/moviereservation/domain/Showing.java
+++ src/main/java/org/akanick/moviereservation/domain/Showing.java
@@ -2,6 +2,7 @@ package org.akanick.moviereservation.domain;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -16,7 +17,7 @@ public class Showing {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "movie_id")
     private Movie movie;
 
