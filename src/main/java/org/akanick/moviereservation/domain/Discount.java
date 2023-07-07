10c906006385373b9e0e0305dc87a1ac4ffcced8
aka-nick <bechalrang@gmail.com>
2023-07-07 13:53:57 +0900

    트랜잭션 스크립트 - 빈약한 도메인 객체 - Discount domain 추가

---
+++ src/main/java/org/akanick/moviereservation/domain/Discount.java
@@ -0,0 +1,39 @@
+package org.akanick.moviereservation.domain;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Discount {
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id; // 예시에서는 Movie-Discount가 비식별관계지만, 편의상 식별관계로 변경한다.
+
+    @ManyToOne
+    @JoinColumn(name = "movie_id")
+    private Movie movie;
+
+    @Column(name = "discount_type")
+    private DiscountType discountType;
+
+    @Column(name = "fee_amount")
+    private Integer feeAmount;
+
+    @Column(name = "fee_currency")
+    private Integer feeCurrency;
+
+    @Column(name = "percent")
+    private Double percent;
+
+    enum DiscountType {
+        AMOUNT, PERCENT
+    }
+
+}
